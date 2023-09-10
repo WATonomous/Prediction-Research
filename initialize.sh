ab0b868160240fbaf882c652d465925053b1d4bb
Eddy Zhou <e23zhou@uwaterloo.ca>
2023-09-10 14:55:35 +0000

    changed project name

--- initialize.sh
+++ initialize.sh
@@ -3,6 +3,6 @@ FIXUID=$(id -u)
 FIXGID=$(id -g)
 
 > ".env"
-echo "COMPOSE_PROJECT_NAME=perception-research-${USER}" >> ".env"
+echo "COMPOSE_PROJECT_NAME=prediction-research-${USER}" >> ".env"
 echo "FIXUID=$FIXUID" >> ".env"
 echo "FIXGID=$FIXGID" >> ".env"
